cc588ac1c6d16623b665b3f6b230407e24c10e10
Gilberto Bertin <jibi@cilium.io>
2022-05-25 11:54:43 +0200

    logfields: add SPI and OldSPI IPSec constants
    [ upstream commit 604ec254cca5057bac189a7b30e123399f6015a5 ]
    
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- pkg/datapath/linux/ipsec/ipsec_linux.go
+++ pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -729,7 +729,7 @@ func ipSecSPICanBeReclaimed(spi uint8, reclaimTimestamp time.Time) bool {
 }
 
 func deleteStaleXfrmStates(reclaimTimestamp time.Time) {
-	scopedLog := log.WithField("spi", ipSecCurrentKeySPI)
+	scopedLog := log.WithField(logfields.SPI, ipSecCurrentKeySPI)
 
 	xfrmStateList, err := netlink.XfrmStateList(0)
 	if err != nil {
@@ -744,7 +744,7 @@ func deleteStaleXfrmStates(reclaimTimestamp time.Time) {
 			continue
 		}
 
-		scopedLog = log.WithField("oldSPI", stateSPI)
+		scopedLog = log.WithField(logfields.OldSPI, stateSPI)
 
 		scopedLog.Info("Deleting stale XFRM state")
 		if err := netlink.XfrmStateDel(&s); err != nil {
@@ -754,7 +754,7 @@ func deleteStaleXfrmStates(reclaimTimestamp time.Time) {
 }
 
 func deleteStaleXfrmPolicies(reclaimTimestamp time.Time) {
-	scopedLog := log.WithField("spi", ipSecCurrentKeySPI)
+	scopedLog := log.WithField(logfields.SPI, ipSecCurrentKeySPI)
 
 	xfrmPolicyList, err := netlink.XfrmPolicyList(0)
 	if err != nil {
@@ -774,7 +774,7 @@ func deleteStaleXfrmPolicies(reclaimTimestamp time.Time) {
 			continue
 		}
 
-		scopedLog = log.WithField("oldSPI", policySPI)
+		scopedLog = log.WithField(logfields.OldSPI, policySPI)
 
 		scopedLog.Info("Deleting stale XFRM policy")
 		if err := netlink.XfrmPolicyDel(&p); err != nil {
