zhyoulun/cilium

commit 83fc8f7708c3c81daf1583e5edc32c23356b9515

bpf: nat: fix from_endpoint assignment
[ upstream commit 658045e8a30d070db40ad2c0769dc7a01ed9328c ]

In snat_v4_needed(), whenever NAT is required because of an egress
gateway policy, we unconditionally set the from_endpoint parameter to
true, as egress gateway traffic is always originating from an endpoint.

This is wrong as that parameter is supposed to track whether traffic is
coming from a _local_ endpoint, and so that variable should be set to true
only if the __lookup_ip4_endpoint() lookup is successful.

Signed-off-by: Gilberto Bertin <jibi@cilium.io>
Signed-off-by: Gilberto Bertin <jibi@cilium.io>

bpf/lib/nat.h
@@ -589,6 +589,8 @@ static __always_inline bool snat_v4_needed(struct __ctx_buff *ctx,
                        .saddr = ip4->saddr
                };
 
+	       *from_endpoint = true;
+
                ct_is_reply4(get_ct_map4(&tuple), ctx, ETH_HLEN +
                             ipv4_hdrlen(ip4), &tuple, &is_reply);
        }
@@ -629,7 +631,6 @@ static __always_inline bool snat_v4_needed(struct __ctx_buff *ctx,
 
        target->addr = egress_gw_policy->egress_ip;
        target->egress_gateway = true;
-       *from_endpoint = true;
 
        return true;
 
@@ -683,7 +684,6 @@ skip_egress_gateway:
                 * reply.
                 */
                if (!is_reply && local_ep) {
-                       *from_endpoint = true;
                        target->addr = IPV4_MASQUERADE;
                        return true;
                }

commit d82553e8a10dfe53eb5067b5f4caba433c94f543

bpf: nat: fix snat_v4_can_skip() for egress gateway
After the introduction of 83fc8f7708c3 ("bpf: nat: fix from_endpoint
assignment") snat_v4_can_skip() can incorrectly determine that some
egress gateway traffic should not be SNATed, as the from_endpoint
parameter of snat_v4_needed() is not set unconditionally anymore when
a packet matches an egress gateway policy.

This commit fixes this by explicitly handling the egress gateway case.

Fixes: #23173
Fixes: 83fc8f7708c3 ("bpf: nat: fix from_endpoint assignment")
Signed-off-by: Gilberto Bertin <jibi@cilium.io>

bpf/lib/nat.h
@@ -506,6 +506,9 @@ snat_v4_nat_can_skip(const struct ipv4_nat_target *target, const struct ipv4_ct_
 {
 	__u16 sport = bpf_ntohs(tuple->sport);
 
+	if (target->egress_gateway)
+		return false;
+
 	return (!target->from_local_endpoint && !target->src_from_world &&
 		sport < NAT_MIN_EGRESS) ||
 		icmp_echoreply;